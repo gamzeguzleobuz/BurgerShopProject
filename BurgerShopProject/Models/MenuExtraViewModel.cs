709653d56158548b496d368285201995f5986eaa
yasinn gürbüz <yasingurbuz1@hotmail.com>
2023-08-30 16:46:46 +0300

    Yasin2

--- BurgerShopProject/Models/MenuExtraViewModel.cs
+++ BurgerShopProject/Models/MenuExtraViewModel.cs
@@ -4,12 +4,7 @@ namespace BurgerShopProject.Models
 {
     public class MenuExtraViewModel
     {
-
         public List<Menu> Menu { get; set; }
-
-
         public List<Extra> Extra { get; set; }
-
-
     }
 }
